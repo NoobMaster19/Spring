b8c5e52960e0d3d248669e46f66081b2eeb51087
Gopal <Gopal@DESKTOP-T86POTJ>
2019-12-07 18:45:31 +0530

    new change

---
+++ Spring-demo-annotations/src/MyLoggerConfig/SpringDemo/log.java
@@ -0,0 +1,10 @@
+package MyLoggerConfig.SpringDemo;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("MyLoggerConfig.SpringDemo")
+public class log {
+
+}

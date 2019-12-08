75d9617938c55e260ca8fbc4976006565f17278c
NoobMaster19 <53575610+NoobMaster19@users.noreply.github.com>
2019-12-08 19:04:13 +0530

    New Update

--- Spring-demo-annotations/src/com/luv2code/springdemo/SwimCoach.java
+++ Spring-demo-annotations/src/com/luv2code/springdemo/SwimCoach.java
@@ -2,6 +2,7 @@ package com.luv2code.springdemo;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 
@@ -10,6 +11,13 @@ public class SwimCoach implements Coach {
 	
 	private FortuneService fortuneService;
 	
+	
+	//Field level injection
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
 	public SwimCoach(FortuneService theFortuneService)
 	{
 		fortuneService=theFortuneService;
@@ -27,5 +35,17 @@ public class SwimCoach implements Coach {
 		// TODO Auto-generated method stub
 		return fortuneService.getDailyFortune();
 	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public String getTeam() {
+		return team;
+	}
+	
+	
 
 }
